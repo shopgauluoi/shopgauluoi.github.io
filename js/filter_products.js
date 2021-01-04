6cc4b569ef9c465576ee3da3d754ddae16150918
cnguyen10 <cuong.c.nguyen@hotmail.com>
2021-01-04 22:49:47 +1030

    Upgrade into multiple collections for product categories Update js search

--- js/filter_products.js
+++ js/filter_products.js
@@ -1,15 +1,31 @@
 function filterFunction() {
-  var input, filter, cards, cardContainer, h5, title, i;
-  input = document.getElementById("input-search");
-  filter = input.value.toUpperCase();
-  cardContainer = document.getElementById("grid");
-  cards = cardContainer.getElementsByClassName("product-item");
-  for (i = 0; i < cards.length; i++) {
-      title = cards[i].querySelector(".card-product-name");
-      if (title.innerText.toUpperCase().indexOf(filter) > -1) {
-          cards[i].style.display = "";
-      } else {
-          cards[i].style.display = "none";
-      }
-  }
+    var input, filter, cards, cardContainer, title;
+    var productBrands;
+
+    input = document.getElementById("input-search");
+    filter = input.value.toUpperCase();
+    cards = document.getElementsByClassName("product-item");
+    productBrands = document.getElementsByClassName("product-brand");
+
+    for (var i = 0; i < cards.length; i++) {
+        title = cards[i].querySelector(".card-product-name");
+        if (title.innerText.toUpperCase().indexOf(filter) > -1) {
+            cards[i].style.display = "";
+        } else {
+            cards[i].style.display = "none";
+        }
+    }
+
+    if (filter.length == "") {
+        show_hide_product_brand(productBrands, "");
+    } else {
+        show_hide_product_brand(productBrands, "none");
+    }
+}
+
+function show_hide_product_brand(productBrands, flag) {
+    // flag: "" or "none"
+    for (var i = 0; i < productBrands.length; i++) {
+        productBrands[i].style.display = flag;
+    }
 }
